2083f18435d27187d23a2b2088a81683e14400b4
julian2077 <beg2055js@gmail.com>
2020-06-21 16:52:16 +0300

    1 commit react lesson 13

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Heder';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar />
+            <Profile />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                {/* <img src='https://images.pexels.com/photos/972391/pexels-photo-972391.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'></img> */}
+                <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'></img>
+            </div>
+            <div> ava + description </div>
+            <div> my posts
+            <div> new posts</div>
+                <div>
+                    <div>post 111</div>
+                    <div>post 221</div>
+                    <div>post 331</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
